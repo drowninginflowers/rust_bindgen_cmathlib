f4df4e9fa5813f936c8f927f918ec918f5eb3f75
drowninginflowers <jordyn.geiger1@gmail.com>
2025-04-10 15:27:38 -0400

    define mathlib functions

---
+++ c_mathlib/mathlib.c
@@ -0,0 +1,18 @@
+#include "mathlib.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+double divide(int a, int b) {
+    if (b == 0) return 0.0; // ignore divide by zero
+    return (double)a / b;
+}
